6a7ba65ba38061c50e22ae82b80d60fdabfbba2d
Ronando <ronando.long@gmail.com>
2016-02-22 20:49:15 -0800

    Update app.php

--- app/app.php
+++ app/app.php
@@ -2,12 +2,12 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Example.php";
 
-    session_start();
-    if (empty($_SESSION['list_of_places'])) {
-        $_SESSION['list_of_places'] = array();
-    }
-
     $app = new Silex\Application();
+    
+    $server = 'mysql:host=localhost;dbname=to_do';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
 
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
     'twig.path' => __DIR__.'/../views'
